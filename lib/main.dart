0ce7750d598f6f09d87219f5406e72ddf8149c05
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-04-25 23:22:44 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s create a shop?'),
+      ),
+    );
+  }
+}
