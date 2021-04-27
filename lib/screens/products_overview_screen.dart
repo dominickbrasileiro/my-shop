905c8b2b4751642fb05d2675eee8c8dca5e9fa35
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-04-27 16:25:50 -0300

    Extract Product Grid Widget

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:fshop/providers/products_provider.dart';
-import 'package:fshop/widgets/product_item_widget.dart';
-import 'package:provider/provider.dart';
+import 'package:fshop/widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<ProductsProvider>(context);
-    final products = productsProvider.products;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 3 / 2,
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItemWidget(
-          product: products[i],
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fshop/providers/products_provider.dart';
+import 'package:fshop/widgets/product_item_widget.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final products = productsProvider.products;
+
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 3 / 2,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ProductItemWidget(
+        product: products[i],
+      ),
+    );
+  }
+}
