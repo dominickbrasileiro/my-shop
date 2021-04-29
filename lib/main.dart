fd8a58d82d8b8cb985d3d6d51fb78076c516a925
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-04-28 23:44:49 -0300

    Add MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fshop/core/app_routes.dart';
+import 'package:fshop/providers/cart_provider.dart';
 import 'package:fshop/providers/products_provider.dart';
 import 'package:fshop/screens/product_details_screen.dart';
 import 'package:fshop/screens/products_overview_screen.dart';
@@ -10,8 +11,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => CartProvider(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
