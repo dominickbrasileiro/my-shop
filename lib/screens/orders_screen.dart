4aa232667f36353a7e99320ff842aac2043b551e
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-07 18:34:55 -0300

    Add FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,51 +4,45 @@ import 'package:fshop/widgets/app_drawer.dart';
 import 'package:fshop/widgets/order_item_widget.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  Future<void> _fetchOrders() async {
-    await Provider.of<OrdersProvider>(context, listen: false).fetchOrders();
+class OrdersScreen extends StatelessWidget {
+  Future<void> _fetchOrders(ctx) async {
+    await Provider.of<OrdersProvider>(ctx, listen: false).fetchOrders();
   }
 
   @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<OrdersProvider>(context);
-    final orders = ordersProvider.orders;
-
     return Scaffold(
       drawer: AppDrawer(),
       appBar: AppBar(
         title: Text('My Orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: _fetchOrders(context),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: _fetchOrders,
-              child: ListView.builder(
-                itemCount: ordersProvider.itemCount,
-                itemBuilder: (ctx, i) => OrderItemWidget(
-                  order: orders[i],
+            );
+          } else if (snapshot.error != null) {
+            return Center(
+              child:
+                  Text('An unexpected error ocurred. Please try again later.'),
+            );
+          } else {
+            return RefreshIndicator(
+              onRefresh: () => _fetchOrders(context),
+              child: Consumer<OrdersProvider>(
+                builder: (ctx, ordersProvider, child) => ListView.builder(
+                  itemCount: ordersProvider.itemCount,
+                  itemBuilder: (ctx, i) => OrderItemWidget(
+                    order: ordersProvider.orders[i],
+                  ),
                 ),
               ),
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
