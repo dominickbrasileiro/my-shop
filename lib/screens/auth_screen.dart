71d2552b2a6ad8d9fbf099d2432b2bec8f753930
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-21 20:46:29 -0300

    Correct responsivity in auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -21,40 +21,47 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 6,
-                  ),
-                  transform: Matrix4.rotationZ(pi / -24)..translate(-10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.green,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 6,
                       ),
-                    ],
-                  ),
-                  child: Text(
-                    'My Shop',
-                    style: TextStyle(
-                      color: Theme.of(context).accentTextTheme.headline1?.color,
-                      fontFamily: 'Anton',
-                      fontSize: 45,
+                      transform: Matrix4.rotationZ(pi / -24)..translate(-10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.green,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'My Shop',
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .accentTextTheme
+                              .headline1
+                              ?.color,
+                          fontFamily: 'Anton',
+                          fontSize: 45,
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(height: 20),
+                    AuthCardWidget(),
+                  ],
                 ),
-                SizedBox(height: 20),
-                AuthCardWidget(),
-              ],
+              ),
             ),
           ),
         ],
